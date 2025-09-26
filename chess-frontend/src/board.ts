4322e3a91e8992932404e0d41b785e51346c6d42
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2025-09-26 15:37:38 +0300

    Created chessboard initial version

---
+++ chess-frontend/src/board.ts
@@ -0,0 +1,38 @@
+import "./style.css";
+
+const board = document.querySelector<HTMLDivElement>("#board")!;
+
+board.innerHTML = `
+  <div class="board-wrapper">
+    <div class="board-numbers"></div>
+    <div class="board-grid"></div>
+  </div>
+  <div class="board-text"></div>
+`;
+
+const boardGrid = document.querySelector<HTMLDivElement>(".board-grid")!;
+const boardText = document.querySelector<HTMLDivElement>(".board-text")!;
+const boardNumbers = document.querySelector<HTMLDivElement>(".board-numbers")!;
+
+const boardValues = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+boardValues.forEach((value) => {
+  const textBox = document.createElement("p");
+  textBox.textContent = value;
+  boardText.appendChild(textBox);
+});
+
+for (let row = 0; row < 8; row++) {
+  const numberBox = document.createElement("p");
+  numberBox.textContent = (8 - row).toString();
+  for (let col = 0; col < 8; col++) {
+    const square = document.createElement("div");
+    if ((row + col) % 2 === 0) {
+      square.className = "board-primary";
+    } else {
+      square.className = "board-secondary";
+    }
+    boardGrid.appendChild(square);
+  }
+  boardNumbers.appendChild(numberBox);
+}
